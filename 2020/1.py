6f93e5adeb384615cb08a951f4ebad0b3fa238d7
arc <naurthal0@gmail.com>
2020-12-02 02:39:11 +0100

    2020-01-py,scm

---
+++ 2020/1.py
@@ -0,0 +1,16 @@
+xs = [int(x) for x in open('1.input').read().splitlines()]
+ys = [False] * 2020
+
+for x in xs:
+    if ys[2020 - x]:
+        y = 2020 - x
+        print(x * y)
+        break
+    ys[x] = True
+
+for x in xs:
+    for y in xs:
+        z = 2020 - x - y
+        if z < 0 or not ys[z]:
+            continue
+        print(x, y, z, x*y*z)
---
+++ 2020/1.scm
@@ -0,0 +1,34 @@
+(import (scheme))
+
+(define xs
+  (map string->number
+       (let ([in (open-input-file "1.input")])
+         (let loop ()
+           (let ([line (get-line in)])
+             (if (eof-object? line) '()
+                 (cons line (loop))))))))
+(define ys (make-bytevector 2020 0))
+(for-each (lambda (x) (bytevector-u8-set! ys x 1)) xs)
+
+(for-each
+ (lambda (x)
+   (let ([y (- 2020 x)])
+     (when (= 1 (bytevector-u8-ref ys y))
+       (display (* x y)) (newline))))
+ xs)
+
+(time
+ (let loop ([x (car xs)] [rest (cdr xs)])
+   (for-each
+    (lambda (y)
+      (let ([z (fx- 2020 x y)])
+        (when (and
+               (fx< 0 z)
+               (= 1 (bytevector-u8-ref ys z)))
+          (display (fx* x y z)) (newline)))
+      )
+    rest)
+   (when (not (null? (cdr rest)))
+     (loop (car rest) (cdr rest))))
+ )
+
